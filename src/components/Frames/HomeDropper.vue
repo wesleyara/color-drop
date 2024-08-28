e85bfc6952505bcfd19d5d5d9f717d9c9053ef5c
wesleyara <wes1gom@gmail.com>
2024-08-28 10:16:25 -0300

    fix: change display text

---
+++ src/components/Frames/HomeDropper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useEyeDropper } from "@vueuse/core";
+
+const { isSupported, open, sRGBHex } = useEyeDropper();
+
+const handleDrop = async () => {
+  await open();
+};
+</script>
+
+<template>
+  <button :disabled="!isSupported" @click="handleDrop">
+    <v-icon class="h-8 w-8" name="bi-eyedropper" />
+  </button>
+
+  <span>{{ sRGBHex }}</span>
+</template>
